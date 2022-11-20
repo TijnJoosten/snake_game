7f1e94df2770bff906f642aca00580e9a4802363
TijnJoosten <tijnjoosten@gmail.com>
2022-11-20 15:43:02 +0100

    Added docstrings to the pathing snake class

--- snakes/pathing_snake.py
+++ snakes/pathing_snake.py
@@ -1,7 +1,19 @@
 from snakes.snake import Snake
 
 class Pathing_Snake(Snake):
+    """Basic pathing snake class which first creates a path using random steps before taking the steps."""
+
     def __init__(self, x:int=0, y:int=0, size:int=3, pathing_iterations:int=5):
+        """Initialises the basic pathing snake class.
+
+        Args:
+            x (int): Starting x-coordinate of the head of the snake. Defaults to 0.
+            y (int): Starting y-coordinate of the head of the snake. Defaults to 0.
+            size (int): The total sizez of the snake including the head. Defaults to 3.
+            pathing_iterations (int): Number of iterations the snake will compute the path. 
+                                      If max has been reached, the snake will follow this path. Defaults to 5.
+        """
+
         super().__init__(x, y, size)
         self.pathing = True
         self.path = []
@@ -10,15 +22,24 @@ class Pathing_Snake(Snake):
         self.pathing_iterations_origin = pathing_iterations
     
     def make_aware(self, world):
+        """Gives the snake full access to the game world.
+
+        Used to give the snake knowledge about the world such that it can make better decisions.
+
+        Args:
+            world (World): The game world in which the snake lives.
+        """
+
         self.world = world
 
     def next_step(self):
         """Determines the next step the snake will take.
 
-        This snake will choose the next step randomly.
+        First, the snake will create a path containing the steps. 
+        Afterwards it will follow this path by taking the consecutive steps.
 
         Returns:
-            Step: the next step the snake wants to take.
+            Step: the next step the snake it is going to take or the step it wants to take at a given coordinate.
         """
         
         if self.pathing:
